8060db79b543111223e495a70141b4910d77927f
InstantChaos <fnbeddie@hotmail.com>
2015-10-24 23:32:36 -0400

    Update 1
    Added comments and uploaded files

---
+++ Assignement2Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,128 @@
+﻿//Source File: PlayerController
+//Author: Franco Chong
+//Date Modified: October 21, 2015
+//Program Description: Lets the player control the movement of the character.
+
+using UnityEngine;
+using System.Collections;
+
+//public classes that can be reused.
+[System.Serializable]
+public class VelocityRange
+{
+	public float vMin, vMax;
+
+	public VelocityRange (float vMin, float vMax)
+	{
+		this.vMin = vMin;
+		this.vMax = vMax;
+	}
+}
+
+public class PlayerController : MonoBehaviour
+{
+
+	//public variables 
+	public float speed = 20f;
+	public float jump = 200f;
+	public VelocityRange velocityRange = new VelocityRange (300f, 1000f);
+	
+	//private variables
+	private Rigidbody2D rigid;
+	private Animator animator;
+	private Transform trans;
+
+
+	//sets the value to check if the player is moving.
+	private float moveVal = 0;
+	//checks if the player is facing right.
+	private bool facingright = true;
+	//checks if the player is on the ground.
+	private bool grounded = true;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		this.rigid = gameObject.GetComponent<Rigidbody2D> ();
+		this.trans = gameObject.GetComponent<Transform> ();
+		this.animator = gameObject.GetComponent<Animator> ();		
+
+		
+	}
+	
+	void FixedUpdate ()
+	{
+		//variables to set the position of the forces
+		float forceX = 0f;
+		float forceY = 0f;
+
+		//sets the velocity with an absolute value.
+		float velX = Mathf.Abs (this.rigid.velocity.x);
+		float velY = Mathf.Abs (this.rigid.velocity.y);
+		
+		this.moveVal = Input.GetAxis ("Horizontal");
+
+		//checks if the player is moving
+		if (this.moveVal != 0) { 
+			//check if player is on the ground
+			if (this.grounded) {
+				this.animator.SetInteger ("AnimState", 1);
+				if (this.moveVal > 0) {
+					// moves the player to the right
+					if (velX < this.velocityRange.vMax) {
+						forceX = this.speed;
+						this.facingright = true;
+						this.flip ();
+					}
+				} else if (this.moveVal < 0) {
+					// moves the player to the left
+					if (velX < this.velocityRange.vMax) {
+						forceX = -this.speed;
+						this.facingright = false;
+						this.flip ();
+					}
+				}
+			}
+			
+			
+		} else {
+			// plays the player's idle animation.
+			this.animator.SetInteger ("AnimState", 0);
+		}
+		
+		// check if player is jumping using the WASD keys on the keyboard
+		if ((Input.GetKey (KeyCode.W))) {
+			// checks if the player is on the ground
+			if (this.grounded) {
+				//plays the player's jump animation.
+				this.animator.SetInteger ("AnimState", 2);
+				if (velY < this.velocityRange.vMax) {
+					forceY = this.jump;
+					this.grounded = false;
+				}
+			}
+		}
+		
+		// pushes the player with the force variables set above.
+		this.rigid.AddForce (new Vector2 (forceX, forceY));
+	}
+	
+	void OnCollisionStay2D (Collision2D otherCollider)
+	{
+		if (otherCollider.gameObject.CompareTag ("Platform")) {
+			this.grounded = true;
+		}
+	}
+	
+	// private methods
+	//flips the player's sprite to face the other side when a value either -1 or 1.
+	private void flip ()
+	{
+		if (this.facingright) {
+			this.trans.localScale = new Vector3 (3f, 3f, 1f);
+		} else {
+			this.trans.localScale = new Vector3 (-3f, 3f, 1f);
+		}
+	}
+	
+}
---
+++ Assignement2Platformer/Assets/Scripts/SpawnPlatforms.cs
@@ -0,0 +1,51 @@
+﻿//Source File: SpawnPlatforms
+//Author: Franco Chong
+//Date Modified: October 22, 2015
+//Program Description: Spawns platforms across the game scene.
+
+using UnityEngine;
+using System.Collections;
+
+public class SpawnPlatforms : MonoBehaviour {
+
+	//public variables
+
+	//spawns the amount of platforms at a time.
+	public int platformspawned = 10;
+	//allows the game to spawn a platform away from the player at a specified location.
+	public float minHorizontal = 5f;
+	public float maxHorizontal = 10f;
+	public float minVertical = -5f;
+	public float maxVertical = 5f;
+
+	public GameObject platform;
+
+	//private variables
+
+	//sets the position for the first platform
+	private Vector2 oPosition;
+
+	// Use this for initialization
+	void Start () {
+
+		//gets the value of the first platform
+		oPosition = transform.position;
+		Spawningtheplatforms ();
+	
+	}
+
+	//method to spawn the platforms randomly within a set value
+	void Spawningtheplatforms(){
+		for (int i = 0; i < platformspawned; i++)
+		{
+			Vector2 random = oPosition + new Vector2 (Random.Range(minHorizontal, maxHorizontal), Random.Range (minVertical, maxVertical));
+			Instantiate(platform, random, Quaternion.identity);
+			oPosition = random;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
